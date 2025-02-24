3c525a94447ed4a46303aab3f2bc8af2b913c37f
Sale1995 <36651129+Sale1995@users.noreply.github.com>
2025-02-24 23:23:38 +0100

    Add files via upload

--- #5/domaci.php
+++ #5/domaci.php
@@ -1,1 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Kalkulator cene</title>
+</head>
+<body>
 
+    <form action="domaci_kalkulacija.php" method="GET">
+
+        <input type="text" placeholder="Unesite cenu proizvoda" name="cena_proizvoda">
+        <br>
+        
+        <select name="Vrsta_robe">
+            <option value="Hrana">Hrana</option>
+            <option value="Oprema za racunare">Oprema za racunare</option>
+        </select><br>
+        
+        <label>
+            <input type="checkbox" name="Porez"> Izračunaj porez
+        </label><br>
+
+        <button>Izračunaj cenu</button>
+    </form>
+
+</body>
+</html>
