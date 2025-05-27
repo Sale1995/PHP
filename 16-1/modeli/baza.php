229c3c2f534ed98bff0fb197fca14c66761dc8ed
Sale1995 <duncicaleksandar@gmail.com>
2025-05-27 21:28:23 +0200

    Create .gitattributes

--- 16-1/modeli/baza.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $baza = mysqli_connect("localhost", "root", "root", "online_web");
-
-    if(mysqli_connect_error())
-    {
-        die("Konekcija nije uspela, doslo je do greske");
-    }
--- 17-1/modeli/baza.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $baza = mysqli_connect("localhost", "root", "root", "web_shop");
-
-    if(mysqli_connect_error())
-    {
-        die("Doslo je do greske prilikom povezivanja sa bazom");
-    }
